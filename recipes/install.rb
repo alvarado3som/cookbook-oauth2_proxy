bf283f7f2fc8faaaa420c93ed27f7b1a0b8e5bcc
Matthieu Serrepuy <lotooo@gmail.com>
2017-06-16 12:27:04 +0200

    Build oauth2 from sources

--- recipes/install.rb
+++ recipes/install.rb
@@ -10,11 +10,31 @@
 include_recipe 'ark'
 include_recipe 'pleaserun::default'
 
-ark 'oauth2_proxy' do
-  url node['oauth2_proxy']['install_url']
-  checksum node['oauth2_proxy']['checksum']
-  path node['oauth2_proxy']['install_path']
-  action :install
+#ark 'oauth2_proxy' do
+#  url node['oauth2_proxy']['install_url']
+#  checksum node['oauth2_proxy']['checksum']
+#  path node['oauth2_proxy']['install_path']
+#  action :install
+#end
+
+### Let's build the version from the source
+package "golang-go"
+directory "/tmp/go/src/github/bitly" do
+    recursive true
+end
+
+git 'oauth2-proxy' do
+    destination "/tmp/go/src/github/bitly/oauth2_proxy"
+    repository  "https://github.com/kindlyops/oauth2_proxy.git"
+    revision    "github-teams-tweaks"
+    notifies    :run, "execute[Compile oauth2_proxy]", :immediate
+end
+
+execute "Compile oauth2_proxy" do
+    command     "go get"
+    environment ({ GOPATH="/tmp/go"})
+    cwd         "/tmp/go/src/github/bitly/oauth2_proxy"
+    action      :nothing
 end
 
 directory node['oauth2_proxy']['config_files'] do
