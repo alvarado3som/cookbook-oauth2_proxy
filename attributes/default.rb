3742be5d6723baef609aaa511602a888ae5c2d7b
Matthieu Serrepuy <lotooo@gmail.com>
2018-08-15 13:20:17 +0200

    Bump default installed version to 2.2.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@
 # Source:: https://github.com/orion-cookbooks/oauth2_proxy
 #
 
-default['oauth2_proxy']['install_url']  = 'https://github.com/bitly/oauth2_proxy/releases/download/v2.0.1/oauth2_proxy-2.0.1.linux-amd64.go1.4.2.tar.gz'
-default['oauth2_proxy']['checksum']     = 'c6d8f6d74e1958ce1688f3cf7d60648b9d0d6d4344d74c740c515a00b4e023ad'
+default['oauth2_proxy']['install_url']  = 'https://github.com/bitly/oauth2_proxy/releases/download/v2.2/oauth2_proxy-2.2.0.linux-amd64.go1.8.1.tar.gz'
+default['oauth2_proxy']['checksum']     = '1c16698ed0c85aa47aeb80e608f723835d9d1a8b98bd9ae36a514826b3acce56'
 default['oauth2_proxy']['install_path'] = '/usr/local/oauth2_proxy'
 
 default['oauth2_proxy']['config_files'] = '/etc/oauth2_proxy/'
