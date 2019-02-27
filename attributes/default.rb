364e9ef17de06b940660c76cb334f038cfc79505
Matthieu Serrepuy <lotooo@gmail.com>
2019-02-27 14:00:28 +0100

    Fix checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,6 +11,6 @@
 #default['oauth2_proxy']['checksum']     = '1c16698ed0c85aa47aeb80e608f723835d9d1a8b98bd9ae36a514826b3acce56'
 default['oauth2_proxy']['install_url']  = 'https://github.com/pusher/oauth2_proxy/releases/download/v3.1.0/oauth2_proxy-v3.1.0.linux-amd64.go1.11.tar.gz'
 default['oauth2_proxy']['install_path'] = '/usr/local/oauth2_proxy'
-default['oauth2_proxy']['checksum']     = 'ebc896d730b3829bd851b7b43c055627fb2954fe5ab96cd6f015306d763292a1'
+default['oauth2_proxy']['checksum']     = 'adef7be37522b3a8b2a984402c565498da280c518d4d34ed3698274098d55472'
 default['oauth2_proxy']['config_files'] = '/etc/oauth2_proxy/'
 default['oauth2_proxy']['go_path']      = '/tmp/go'
